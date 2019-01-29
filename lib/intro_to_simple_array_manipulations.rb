09e0217bc4e97078e0306d47122055b8093ddaff
Mickey <micksheridan.24@gmail.com>
2019-01-28 19:43:04 -0500

    s

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push (array, str)
+  array.push (str)
+end
+
+def using_unshift (array, st)
+  array.unshift (st)
+end
+
+def using_pop (array)
+  array.pop
+end
+
+def using_shift (array)
+  array.shift
+end
+
+def shift_with_args (array)
+  array.shift (2)
+end
+
+def pop_with_args (array)
+  array.pop (2)
+end
+
+def using_concat (array, array2)
+  array.concat(array2)
+
+end
+
+def using_insert(array, x)
+  array.insert(4, x)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete(array, str)
+  array.delete (str)
+end
+
+def using_delete_at(array, n)
+  array.delete_at (n)
+end
